8c12ad73574e29ea0d4c2a650b0b31bcbd384a82
syed ayeesha <aishu2771@gmail.com>
2019-03-23 15:13:51 +0530

    pwa_phase

---
+++ serviceworker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('sample').then(function(cache) {
+      return cache.addAll([
+
+      ]);
+    }));
+    console.log("sef");
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(resp) {
+      return resp || fetch(event.request).then(function(response) {
+        return caches.open('sample').then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    }));
+});
